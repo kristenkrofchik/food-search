33dd3dd117af1b64feac3cc60519f5481ee22248
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-21 08:33:14 -0600

    working on frontend skeleton. added reacr router & grommet styling

---
+++ frontend/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SiteFooter from '../shared/SiteFooter';
+
+const NotFound = () => {
+    return (
+        <>
+            <p>We can't find the page you are looking for. Please try again.</p>
+            <SiteFooter />
+        </>
+    )
+};
+
+export default NotFound;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Results from './components/Results';
+import Homepage from './components/Homepage';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/results' component={Results} />
+          <Route path='*' component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
