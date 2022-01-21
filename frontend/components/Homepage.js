e67139acd9fa88b61f443bf10ec9fd8ae6107c90
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-21 15:46:06 -0600

    homepage done

---
+++ frontend/components/Homepage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SiteFooter from '../shared/SiteFooter';
+import SearchBox from './SearchBox';
+import { Box, Heading, Main } from 'grommet';
+import './Homepage.css';
+
+const Homepage = () => {
+    return (
+        <>
+            <div>
+                <Main pad="medium">
+                    <Box align="center">
+                        <Heading level="1" responsive="true" textAlign="center" color="dark" >The Meal Maker</Heading>
+                        <Heading level="3" responsive="true" textAlign="center" color="dark">You pick an ingredient, we'll give you a list of things to make!</Heading>
+                    </Box>
+                    <SearchBox />
+                </Main>
+            </div>
+            <SiteFooter />
+        </>
+    )
+};
+
+export default Homepage;
