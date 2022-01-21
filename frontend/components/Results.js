bbd2f2f3bf2a9910b5dc34a495711dbf07fc9099
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-21 15:56:03 -0600

    basic results page so can test what i have

---
+++ frontend/components/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SiteFooter from '../shared/SiteFooter';
+
+const Results = () => {
+    return (
+        <>
+            <SiteFooter />
+        </>
+    )
+};
+
+export default Results;
