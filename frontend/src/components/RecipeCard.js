7968ed111c25345637ceb5d41fb08f9a67ab7e84
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-23 13:05:50 -0600

    receipe card test

--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -1,9 +1,19 @@
-import React from 'react';
-//import { Button, Card, CardBody, CardHeader, CardFooter, ResponsiveContext } from 'grommet';
+import React, {useContext} from 'react';
+import { Card, CardBody, CardHeader, Image, ResponsiveContext } from 'grommet';
 
 const RecipeCard = (result) => {
-    <>
-    </>
+    const { title, image } = result;
+
+    const size = useContext(ResponsiveContext);
+
+    return (
+        <Card height='medium' width={size} background='light-3'>
+            <CardHeader pad='medium'>{title}</CardHeader>
+            <CardBody pad='medium'>
+                <Image fit='contain' src={image} />
+            </CardBody>
+        </Card>
+    )
 }
 
 export default RecipeCard;
